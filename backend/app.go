fe9b7f1a704a8b0712447b19df0a08199ee80b37
dkaps125 <daniel.kapit05@gmail.com>
2025-12-25 16:45:19 -0700

    Shared system prompt; prepping for monorepo

---
+++ backend/app.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"time"
+
+	"github.com/caseymrm/menuet"
+	"github.com/dkaps125/juke/inference"
+	"github.com/dkaps125/juke/music"
+)
+
+// App is the main app object
+type App struct {
+	music music.Source
+	llm   inference.Engine
+}
+
+func (a App) InitializeMenubar() {
+
+	menuet.App().Children = func() []menuet.MenuItem {
+		return []menuet.MenuItem{
+			{
+				Text:  menuet.Defaults().String("nowPlayingSong"),
+				State: false,
+			},
+			{
+				Text:  menuet.Defaults().String("nowPlayingArtist"),
+				State: false,
+			},
+			{
+				Type: menuet.Separator,
+			},
+			{
+				Text: "Previous",
+				Clicked: func() {
+					a.music.Previous()
+				},
+			},
+			{
+				Text: menuet.Defaults().String("playPause"),
+				Clicked: func() {
+					curr := a.music.CurrentState()
+
+					if curr.Playing {
+						a.music.Pause()
+						menuet.Defaults().SetString("playPause", "Play")
+					} else {
+						a.music.Play()
+						menuet.Defaults().SetString("playPause", "Pause")
+					}
+
+				},
+			},
+			{
+				Text: "Next",
+				Clicked: func() {
+					a.music.Next()
+				},
+			},
+			{
+				Type: menuet.Separator,
+			},
+			{
+				Text: "Juke",
+				Clicked: func() {
+					response := menuet.App().Alert(menuet.Alert{
+						MessageText: "What would you like to hear next?",
+						Inputs:      []string{"Prompt"},
+						Buttons:     []string{"Juke", "Cancel"},
+					})
+
+					if response.Button == 0 && len(response.Inputs) == 1 && response.Inputs[0] != "" {
+						prompt := response.Inputs[0]
+						a.llm.PromptLLM(prompt, &music.Song{
+							Title:  menuet.Defaults().String("nowPlayingSong"),
+							Artist: menuet.Defaults().String("nowPlayingArtist"),
+						}, func(songs []music.Song) {
+							a.music.SearchAndPlaySongs(songs)
+						})
+					}
+				},
+			},
+		}
+	}
+
+	menuet.App().Label = "com.github.dkaps125.juke"
+	menuet.App().SetMenuState(&menuet.MenuState{
+		Title: "Now Playing",
+	})
+}
+
+func (a App) startUpdates() {
+	go func() {
+		for {
+			if a.music == nil {
+				continue
+			}
+
+			curr := a.music.CurrentState()
+
+			if curr.Playing {
+				menuet.Defaults().SetString("playPause", "Pause")
+			} else {
+				menuet.Defaults().SetString("playPause", "Play")
+			}
+
+			if curr.CurrentSong == nil {
+				continue
+			}
+
+			menuet.Defaults().SetString("nowPlayingSong", curr.CurrentSong.Title)
+			menuet.Defaults().SetString("nowPlayingArtist", curr.CurrentSong.Artist)
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+func (a App) Run() {
+	wg, ctx := menuet.App().GracefulShutdownHandles()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		time.Sleep(3 * time.Second)
+	}()
+
+	a.startUpdates()
+	menuet.App().RunApplication()
+}
---
+++ backend/music/spotify.go
@@ -0,0 +1,151 @@
+package music
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dkaps125/juke/utils"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+var redirectURI = os.Getenv("REDIRECT_URI") // TODO: make configurable?
+
+type Spotify struct {
+	client *spotify.Client
+}
+
+var (
+	ch    = make(chan *spotify.Client)
+	state = "abc123" // TODO: make random
+)
+
+func NewSpotify() *Spotify {
+	return &Spotify{}
+}
+
+func (s *Spotify) Authenticate() {
+	srv := &http.Server{Addr: ":8080"}
+
+	http.HandleFunc("/callback", s.completeAuth(srv))
+
+	srv.ListenAndServe()
+}
+
+func (s *Spotify) searchSong(song Song) (*spotify.URI, bool) {
+	results, err := s.client.Search(context.Background(), fmt.Sprintf("track:%s artist:%s", song.Title, song.Artist), spotify.SearchTypeTrack)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, false
+	}
+
+	return &results.Tracks.Tracks[0].URI, true
+}
+
+func (s *Spotify) SearchAndPlaySongs(songs []Song) {
+	var uris = make([]spotify.URI, 0)
+
+	for _, song := range songs {
+		if uri, ok := s.searchSong(song); ok {
+			uris = append(uris, *uri)
+		}
+	}
+
+	s.client.PlayOpt(context.Background(), &spotify.PlayOptions{
+		URIs: uris,
+	})
+}
+
+func (s Spotify) Pause() {
+	s.client.Pause(context.Background())
+}
+
+func (s Spotify) Play() {
+	s.client.Play(context.Background())
+}
+
+func (s Spotify) Next() {
+	s.client.Next(context.Background())
+}
+
+func (s Spotify) Previous() {
+	s.client.Previous(context.Background())
+}
+
+func (s Spotify) CurrentState() PlayerState {
+	if s.client == nil {
+		return PlayerState{
+			CurrentSong: nil,
+			Playing:     false,
+		}
+	}
+
+	curr, _ := s.client.PlayerCurrentlyPlaying(context.Background())
+
+	if curr == nil || curr.Item == nil {
+		return PlayerState{
+			CurrentSong: nil,
+			Playing:     false,
+		}
+	}
+
+	var currentSong *Song = nil
+	if curr.Item != nil {
+		artistNames := make([]string, len(curr.Item.Artists))
+		for i, artist := range curr.Item.Artists {
+			artistNames[i] = artist.Name
+		}
+
+		currentSong = &Song{
+			Title:  curr.Item.Name,
+			Artist: strings.Join(artistNames, ", "),
+		}
+	}
+
+	return PlayerState{
+		CurrentSong: currentSong,
+		Playing:     curr.Playing,
+	}
+}
+
+func (s *Spotify) completeAuth(srv *http.Server) func(w http.ResponseWriter, r *http.Request) {
+	auth := spotifyauth.New(
+		spotifyauth.WithClientID(os.Getenv("SPOTIFY_ID")),
+		spotifyauth.WithClientSecret(os.Getenv("SPOTIFY_SECRET")),
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadCurrentlyPlaying, spotifyauth.ScopeUserReadPlaybackState, spotifyauth.ScopeUserModifyPlaybackState),
+	)
+
+	// In the background, fetch the auth URL and open it in the browser
+	go func() {
+		url := auth.AuthURL(state)
+
+		utils.OpenURL(url)
+	}()
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tok, err := auth.Token(r.Context(), state, r)
+		if err != nil {
+			http.Error(w, "Couldn't get token", http.StatusForbidden)
+			log.Fatal(err)
+		}
+		if st := r.FormValue("state"); st != state {
+			http.NotFound(w, r)
+			log.Fatalf("State mismatch: %s != %s\n", st, state)
+		}
+		// use the token to get an authenticated client
+		s.client = spotify.New(auth.Client(r.Context(), tok))
+
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "<script>window.close('','_parent','')</script>")
+
+		time.AfterFunc(time.Second*5, func() {
+			srv.Shutdown(context.Background())
+		})
+	}
+}
